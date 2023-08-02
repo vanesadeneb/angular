ae278eed7d5519e413f31f9b5f1e8ba60acb9f02
vanesavillarreal <vanessa_diesver@hotmail.com>
2023-08-02 14:57:23 -0700

    Adds list and form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 import { AppComponent } from './app.component';
-import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { HeroesModule } from './heroes/heroes.module';
   imports: [
     BrowserModule,
     CounterModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
+
+
+
+@NgModule({
+  declarations: [
+    MainPageComponent,
+    ListComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MainPageComponent
+  ]
+})
+export class DbzModule { }
