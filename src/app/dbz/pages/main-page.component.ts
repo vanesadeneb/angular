60584031dfea33c253e1110f3ffeb96df5c7f551
vanesavillarreal <vanessa_diesver@hotmail.com>
2023-08-03 14:41:12 -0700

    Adds interface and service

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -6,5 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class MainPageComponent {
+  constructor( private dbzService: DbzService ){}
 
+  get characters(): Character[]{
+    return [...this.dbzService.characters];
+  }
+
+  onDeleteCharacter( id: string ): void {
+    this.dbzService.deleteCharacterById(id);
+  }
+
+  onNewCharacter( character: Character ): void{
+    this.dbzService.addCharacter( character );
+  }
 }
