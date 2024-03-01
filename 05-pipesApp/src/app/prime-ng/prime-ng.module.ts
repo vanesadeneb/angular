0267c8bf7bea2dbea4db5961ad6884da965a65c5
Vanesa Villarreal <vanesa.villarreal@fiscalia.sonora.gob.mx>
2024-03-01 14:04:34 -0700

    Adds animations and PrimeNG Menu

---
+++ 05-pipesApp/src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+import { MenuModule } from 'primeng/menu';
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MenuModule
+  ]
+})
+export class PrimeNgModule { }
---
+++ 05-pipesApp/src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent implements OnInit{
+  public menuItems: MenuItem[] | undefined = [];
+
+  ngOnInit(){
+    this.menuItems = [
+      {label: 'New', icon: 'pi pi-fw pi-plus'},
+      {label: 'Open', icon: 'pi pi-fw pi-download'},
+      {label: 'Undo', icon: 'pi pi-fw pi-refresh'}
+    ];
+  }
+}
